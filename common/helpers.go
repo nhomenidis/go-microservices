d2478a53f08efda62bfc38768e76d5d0261343f1
Nikos Chomenidis <nchomenidis@indeavor.com>
2023-10-12 18:58:32 +0300

    Create a common module for reusable helpers

---
+++ common/helpers.go
@@ -0,0 +1,73 @@
+package common
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type JsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func ReadJson(writer http.ResponseWriter, request *http.Request, data interface{}) error {
+	var maxBytes = 1048576
+
+	request.Body = http.MaxBytesReader(writer, request.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(request.Body)
+	err := dec.Decode(data)
+
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("multiple json values are not allowed at body")
+	}
+
+	return nil
+}
+
+// WriteJSON takes a response status code and arbitrary data and writes a json response to the client
+func WriteJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ErrorJSON takes an error, and optionally a response status code, and generates and sends
+// a json error response
+func ErrorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload JsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return WriteJSON(w, statusCode, payload)
+}
